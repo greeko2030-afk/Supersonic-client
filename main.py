ddc8e91698f4b3042c5dfa75ce608cbcea50f239
greeko2030-afk <greeko2030@gmail.com>
2026-07-07 12:19:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def resource_path(relative_path):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
-        # If running from local GitHub clone
+        # If running from local python script
         base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
 
