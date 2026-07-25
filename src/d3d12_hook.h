2e6f65a6164e1f42fd679edac2317e63ffcf8c76
greeko2030-afk <greeko2030@gmail.com>
2026-07-25 16:28:52 +0600

    Update d3d12_hook.h

--- src/d3d12_hook.h
+++ src/d3d12_hook.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <iostream>
 
+class D3D12Engine {
+public:
+    static void Initialize();
+    static void Shutdown();
+private:
+    static ID3D12Device* g_pd3dDevice;
+};
